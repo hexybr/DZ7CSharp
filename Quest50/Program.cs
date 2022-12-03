6fc4ba3d064f0e97dd4b4d6efeee05a94c5d1988
Handbrake brake <handbrake@mail.ru>
2022-12-03 23:12:52 +0300

    Quest50 complete

--- Quest50/Program.cs
+++ Quest50/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает значение элемента в двумерном массиве,
+// и возвращает позицию этого элемента или же указание, что такого элемента нет.
+Console.WriteLine("Введите количество строк:");
+int rows;
+if (!int.TryParse(Console.ReadLine()!, out rows))
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+
+Console.WriteLine("Введите количество столбцов:");
+int columns;
+if (!int.TryParse(Console.ReadLine()!, out columns))
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+
+Console.WriteLine("Введите искомое число: ");
+int digit;
+if (!int.TryParse(Console.ReadLine()!, out digit))
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+int[,] array = new int[rows, columns];
+FillArray(array);
+PrintArray(array);
+ElementSearch(array);
+
+
+void FillArray(int[,] array)
+{
+
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            array[i, k] = rnd.Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] input)
+{
+    Console.WriteLine("Полученный массив: ");
+    for (int i = 0; i < input.GetLength(0); i++)
+    {
+        for (int k = 0; k < input.GetLength(1); k++)
+        {
+            Console.Write(input[i, k] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void ElementSearch(int[,] array)
+{
+    string pos = "Искомого числа в массиве нет";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            if (array[i, k] == digit)
+            {
+                pos = $"Индекс искомого элемента = ({i},{k})";
+                break;
+            }
+        }
+    }
+    Console.WriteLine(pos);
+}
