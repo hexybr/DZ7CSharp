4fbf1b7f34acb03d503d7f66263afeb70c14d61e
Handbrake brake <handbrake@mail.ru>
2022-12-03 20:41:03 +0300

    Quest47 complete

--- Quest47/Program.cs
+++ Quest47/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите количество строк:");
+int rows;
+if (!int.TryParse(Console.ReadLine()!, out rows))
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+Console.WriteLine("Введите количество столбцов:");
+int columns;
+if (!int.TryParse(Console.ReadLine()!, out columns))
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+float[,] array = new float[rows, columns];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(float[,] array)
+{
+ Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int k = 0; k < columns; k++)
+        {
+            array[i, k] = rnd.Next();
+        }
+    }
+}
+void PrintArray(float[,] input)
+{
+    Console.WriteLine("Полученный массив: ");
+    for (int i = 0; i < input.GetLength(0); i++)
+    {
+        for (int k = 0; k < input.GetLength(1); k++)
+        {
+            Console.Write(input[i, k] + " ");
+        }
+        Console.WriteLine();
+    }
+}
